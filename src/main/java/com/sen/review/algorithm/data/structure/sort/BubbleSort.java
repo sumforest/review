24ad86d8fb66757841decc65aff789068e68175d
Sen <12345678>
2020-04-28 23:12:48 +0800

    init commit

---
+++ src/main/java/com/sen/review/algorithm/data/structure/sort/BubbleSort.java
@@ -0,0 +1,48 @@
+package com.sen.review.algorithm.data.structure.sort;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Sen
+ * @Date: 2020/4/28
+ * @Description: 冒泡排序，平均时间复杂度 n^2；最坏时间复杂度 n^2
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        // int[] arr = {3, -1, 9, 10, -2};
+        int[] arr = {3, -1, 9, 10, -2, 0, 15, 13};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 冒泡排序，按照升序排序
+     *
+     * @param arr 数组
+     */
+    public static void bubbleSort(int[] arr) {
+        // 用于优化冒泡排序标记,true表示经过经过一轮排序后有发生交换，如果经过一轮比较后不发生交换，则表明已经数组有序
+        boolean flag = false;
+        // 控制比较的轮数，由冒泡排序特性可知，最多需要比较 arr.length()-1 轮
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 控制需要比较的元素的下标,开始需要比较的轮数arr.length()-1，每一轮减少一个元素
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                // 前一个比后一个大，交换顺序
+                if (arr[j] > arr[j + 1]) {
+                    flag = true;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            // 优化
+            if (!flag) {
+                break;
+            } else {
+                // 重置标记用于下一轮排序做标记
+                flag = false;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/sen/review/algorithm/data/structure/sort/ShellSort.java
@@ -0,0 +1,78 @@
+package com.sen.review.algorithm.data.structure.sort;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Sen
+ * @Date: 2020/4/28
+ * @Description: 希尔排序，时间复杂度 O(n^s),1 < s < 2
+ * 思路：缩小增量
+ * 1.把需要排序的数组分成 arr.length / 2 组,每组步长5
+ * 2.对每一组都进行插入排序
+ * 3.在原来的基础上再分 arr.length / 2 / 2组
+ * 4.对每一组进行插入排序
+ * 5.直到分成 arr.length 组，每组步长1在进行插入排序，最后数组有序
+ */
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        shellSortByMove(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 交换法实现希尔排序
+     * 缺点：效率低
+     *
+     * @param arr 排序数组
+     */
+    private static void shellSortByExchange(int[] arr) {
+        // gap -- 每次分组的步长/数量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 按照步长划分分组,i代表每一组最后元素的下标
+            for (int i = gap; i < arr.length; i++) {
+                // j -- 每组开始元素的下标,对每一组元素进行插入排序
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        // 前一个比后一个大，交换
+                        int temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 希尔排序，移动法实现效率更高
+     *
+     * @param arr
+     */
+    private static void shellSortByMove(int[] arr) {
+        // 确定分组步长
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // i--每一组最后一个元素下标,遍历每一组
+            for (int i = gap; i < arr.length; i++) {
+                // 有序序列最后一个元素
+                int index = i;
+                int value = arr[i];
+                // 前一个比后一个大进行移位
+                if (arr[index] < arr[index - gap]) {
+                    // 移位 3,1,0,9,7 步长2
+                    while (index - gap >= 0 && value < arr[index - gap]) {
+                        // 将有序序列的最后一个元素往后移动一步长位置，腾出空间给待插入元素
+                        // 3,3,0,9,7 步长2
+                        arr[index] = arr[index - gap];
+                        // index往前移动一个步长
+                        index -= gap;
+                    }
+                    // 交换
+                    arr[index] = value;
+                }
+            }
+        }
+    }
+}
+
